eab64a42112643e8a11d630ca70c976d5ef2002b
Alone0696 <ildar23940696@gmail.com>
2020-08-29 00:52:29 +0300

    Добавлена возможность выбора разрешения видео(пока что без звука)

--- main_logic.py
+++ main_logic.py
@@ -5,7 +5,15 @@ import sys,os
 import moviepy.editor as mpe
 
 
-def down(link):
+def down_not_optim(link,resol):
+    try:  
+        yt = YouTube('%s'%link)
+        yt = yt.streams.filter(file_extension='mp4',res='%s'%resol).first()
+        yt.download()
+        ui.link_l.setText('Успешно')
+    except:
+        ui.link_l.setText('Ошибка(неверная ссылка или неверное качество')
+def down_optim(link):
     try:
         yt = YouTube('%s'%link)
         yt = yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first()
@@ -34,7 +42,10 @@ ui.setupUi(Ramz_ild)
 Ramz_ild.show()
 def start():
     if ui.video_check.isChecked() == True:
-        down(ui.link_l.text())
+        if ui.res.currentText() == 'Оптимальное качество':
+            down_optim(ui.link_l.text())
+        else:
+            down_not_optim(ui.link_l.text(),ui.res.currentText())
     if ui.audio_check.isChecked() == True:
         down_sound(ui.link_l.text())
     if ui.video_check.isChecked() == False and ui.audio_check.isChecked() == False:
